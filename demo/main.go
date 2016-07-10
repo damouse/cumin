072e6c6c1144a29532568ab19e854c56d131dc5f
damouse <damouse007@gmail.com>
2016-07-10 01:41:03 -0500

    well it works out of the box. Thats always nice

--- demo/main.go
+++ demo/main.go
@@ -4,22 +4,20 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
+
+	"github.com/damouse/cumin"
 )
 
-func foo() {
-}
+type asdf struct{}
+
+func (a asdf) receiver() {}
+func foo()               {}
 
 func GetFunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
 
-type asdf struct{}
-
-func (a asdf) receiver() {
-
-}
-
-func main() {
+func testnames() {
 	// main.foo
 	fmt.Println("name:", GetFunctionName(foo))
 
@@ -30,4 +28,19 @@ func main() {
 	// main.(asdf).(main.receiver)
 	a := asdf{}
 	fmt.Println("name:", GetFunctionName(a.receiver))
+}
+
+func receive(a int, b string, c float64, d []string, e map[string]interface{}) (string, error) {
+	fmt.Println(a, b, c, d, e)
+	return "done", nil
+}
+
+func main() {
+	res, err := cumin.Cumin(receive, []interface{}{1, "2", float64(3), []string{"4", "5"}, map[string]interface{}{"6": 7}})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
 }
